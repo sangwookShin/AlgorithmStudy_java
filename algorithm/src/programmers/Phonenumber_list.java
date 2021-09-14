fc88a73dd01e07d210ab711a25edb5ce92edd222
sangwookShin <shinsw6455@naver.com>
2021-09-14 17:00:31 +0900

    프로그래머스 전화번호 목록

--- algorithm/src/programmers/Phonenumber_list.java
+++ algorithm/src/programmers/Phonenumber_list.java
@@ -1,5 +1,31 @@
 package programmers;
+import java.util.*;
 
 public class Phonenumber_list {
 
+	public boolean solution(String[] phone_book) {
+        boolean answer = true;
+        
+        //해쉬맵 생성
+        HashMap<String, Integer> map = new HashMap<>();
+        
+        //해쉬맵 값 추가
+        for(int i=0;i<phone_book.length;i++) {
+            map.put(phone_book[i], i);
+        }
+        
+        //값 비교
+        for(int i=0;i<phone_book.length;i++) {
+            
+            for(int j=1;j<phone_book[i].length();j++) {
+
+                if(map.containsKey(phone_book[i].substring(0,j))) {
+                    return false;
+                }
+            }
+   
+        }
+
+        return answer;
+    }
 }
