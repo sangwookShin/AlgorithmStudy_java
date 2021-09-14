7af64fb12514e0cc9d3aaefe4077ef6d76f851d7
sangwookShin <shinsw6455@naver.com>
2021-09-14 19:13:05 +0900

    프로그래머스 구명보트 그리디

---
+++ algorithm/src/programmers/SavePerson_boat.java
@@ -0,0 +1,22 @@
+package programmers;
+
+import java.util.Arrays;
+
+public class SavePerson_boat {
+	public int solution(int[] people, int limit) {
+        int answer = 0;
+        
+        Arrays.sort(people);
+        
+        int min = 0;
+        
+        for(int max=people.length-1;min<=max;max--) {
+            if(people[min] + people[max] <= limit) {
+                min++;
+            }
+            answer++;
+        }
+        
+        return answer;
+    }
+}
